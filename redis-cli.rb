a02100920debcc1b6fd647a25da313dc716f261f
thgrace <thgrace@users.noreply.github.com>
2026-02-12 23:19:49 -0600

    Initial commit: homebrew formula for redis-cli 8.6.0

---
+++ redis-cli.rb
@@ -0,0 +1,16 @@
+class RedisCli < Formula
+  desc "Install the redis-cli only."
+  homepage "https://github.com/thgrace/homebrew-redis-cli"
+  version "8.6.0"
+  sha256 "74261ece988fd2e1526e5aea9f8b9853217d71e2ef2dafaa624ed9579b5f4317"
+  url "https://github.com/redis/redis/archive/#{version}.tar.gz"
+
+  def install
+    system "make redis-cli"
+    bin.install "./src/redis-cli"
+  end
+
+  test do
+    system "#{bin}/redis-cli"
+  end
+end
